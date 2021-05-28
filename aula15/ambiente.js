71b41069369f08834aa1a9092452af07639b37ae
Maurício de Carvalho <mauriciocr223@gmail.com>
2021-05-28 19:38:49 -0300

    Aprendendo variáveis compostas (Arrays).

---
+++ aula15/ambiente.js
@@ -0,0 +1,20 @@
+let num = [5, 2, 8, 9, 3]
+num.sort()
+/* num[4] = 12
+num.push(19)
+num.sort()
+
+console.log(num) 
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O valor do primeiro vetor é ${num[0]}.`) */
+
+/* for (let pos = 0; pos < num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+} */
+
+/* for (let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+} */
+
+// console.log(num.indexOf(8))
+
